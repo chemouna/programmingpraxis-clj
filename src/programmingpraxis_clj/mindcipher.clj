f1ca93d19b666aa3b577e4e69ee5cfa4aa791056
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-12-30 00:18:11 +0100

    mindcipher finding better solutions.

--- src/programmingpraxis_clj/mindcipher.clj
+++ src/programmingpraxis_clj/mindcipher.clj
@@ -71,6 +71,33 @@
 (defn sum-day-2
   []
   (inc (last
-        (take-while #(not= (int (+ (/ % 100) (mod % 100))) (int (/ (mod % 1000) 10)))
-                    (drop-while #(<= % 1978) (range))))))
+        (take-while #(not=
+                      (int (+ (/ % 100) (mod % 100)))
+                      (int (/ (mod % 1000) 10)))
+                    (iterate inc 1979)))))
+
+(sum-day-2)
+
+;; make it better #3 using a take-util written fn
+(defn take-until
+  "Returns a lazy sequence of successive items from coll until
+   (pred item) returns true, including that item. pred must be
+   free of side-effects."
+  [pred coll]
+  (lazy-seq
+   (when-let [s (seq coll)]
+     (if (pred (first s))
+       (cons (first s) nil)
+       (cons (first s) (take-until pred (rest s)))))))
+
+(defn sum-day-3
+  []
+  (last
+     (take-until #(= (int (+ (/ % 100) (mod % 100))) (int (/ (mod % 1000) 10)))
+                    (iterate inc 1979))))
+
+(sum-day-3)
+
+
+
 
