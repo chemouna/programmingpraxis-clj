19d9008ed4c2a38b7d9d16a023aa5b0193308ef6
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-12-29 02:23:20 +0100

    attempt to add some specs.

--- src/programmingpraxis_clj/mindcipher.clj
+++ src/programmingpraxis_clj/mindcipher.clj
@@ -1,20 +1,21 @@
 (ns programmingpraxis-clj.mindcipher
-  (:require [uncomplicate.fluokitten.core :as flc :refer :all]
-            [clojure.string :as s])
+  (:require [clojure.spec :as s]
+            [clojure.spec.gen :as gen]
+            [clojure.spec.test :as stest])
   (:import java.util.Random))
 
 ;; we consider Head : True and Tail : False
 
 (defn random-boolean [] (.nextBoolean (new Random)))
 
-(defn flipUntil
+(defn flip-until
   "Simulates a series of flips"
   [l]
   (count (take-while #(not= % l) (partition 3 (repeatedly random-boolean)))))
 
 (defn day
   [p]
-  (let [l (repeatedly 10000 #(flipUntil p))]
+  (let [l (repeatedly 10000 #(flip-until p))]
     (float (/ (reduce + l) (count l)))))
 
 (def modayFlipNumber (day '(true false true)))
@@ -22,5 +23,16 @@
 (def tuesdayFlipNumber (day '(true false false)))
 
 ;; TODO: create a spec that check that p is a list of booleans
+(s/def ::cointoss (s/or :t true :f false))
 
+(s/fdef flip-until
+        :args (s/coll-of ::cointoss :count 3 :kind boolean?)
+        :ret nat-int?)
 
+;; (s/exercise-fn  `flip-until)
+
+(stest/check `flip-until)
+
+;; (s/exercise-fn `flip-until)
+
+ ;; Args need to be a collection of 3 boolean
